19b84c83712eded63d7c7e4952dfc8ff9c075bf7
Kisgod <zhanghangwei66@gmail.com>
2025-11-23 15:45:16 +0800

    commit fixing bug

--- include/ResponseHelper.h
+++ include/ResponseHelper.h
@@ -1,13 +1,17 @@
 #pragma once
 #include "crow.h"
 
-// 构造带 CORS 头的响应(暂时没有理解)
+// 构造带更宽松 CORS 头的响应
 inline crow::response withCORS(int code, const std::string& body) {
-    crow::response res(code);
-    res.set_header("Access-Control-Allow-Origin", "*");
-    res.set_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-    res.set_header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    crow::response res;
+    res.code = code;
     res.body = body;
-    res.end();
-    return res;
+
+    // 放宽跨域策略
+    res.set_header("Access-Control-Allow-Origin", "*");
+    res.set_header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH");
+    res.set_header("Access-Control-Allow-Headers", "*"); // 允许所有自定义头
+    res.set_header("Access-Control-Allow-Credentials", "true"); // 如果需要带 cookie
+
+    return res;  // 不要调用 res.end()
 }
